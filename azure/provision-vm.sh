bb9368cf7c8472523568dea6ddd53c5f84c56f9f
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2018-06-12 15:59:57 +0200

    azure/provision-vm.sh: Update according to our latest use
    Summary:
    - Add comments
    - Drop what's not needed
    - Need to test it still works though

--- azure/provision-vm.sh
+++ azure/provision-vm.sh
@@ -2,62 +2,72 @@
 
 set -ex
 
+PUPPET_MASTER=pergamon.internal.softwareheritage.org
+APT='apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"'
+
 cd /
 
+# Update the nodes to the latest packages
+apt-get update; $APT dist-upgrade
+
+# default to private instance
+PRIVATE=${1-"private"}
+
+if [ $PRIVATE != 'private' -a $PRIVATE != 'public' ]; then
+    echo "First argument must be either 'public' or 'private', nothing else."
+    exit 1
+fi
+
+# We need to override the default hostname provided by azure when the
+# node is provisioned
 ORIG_HOSTNAME="$(hostname)"
-HOSTNAME=${ORIG_HOSTNAME/-*/}.euwest.azure
-
-IP=$(ip a | grep 192 | awk '{print $2}' | awk -F/ '{print $1}')
-
-apt-get update
-apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade
+if [ $PRIVATE = "private" ]; then
+    # private ip have a fqdn with the azure location
+    # FIXME: Do we need to use a different hostname pattern when in azure?
+    HOSTNAME=${ORIG_HOSTNAME/-*/}.euwest.azure
+    FQDN=${HOSTNAME}.internal.softwareheritage.org
+else
+    # public ones do not need to leak that information
+    HOSTNAME=${ORIG_HOSTNAME/-*/}
+    FQDN=${HOSTNAME}.softwareheritage.org
+fi
 
 echo $HOSTNAME > /etc/hostname
 hostnamectl set-hostname $HOSTNAME
+
+# we need this to circumvent some puppet issues down the line.  The
+# fqdn policy puppet uses does not render the right name (at least for
+# public node). Puppet is trying to solve the fqdn through the
+# /etc/resolv.conf if not provided in /etc/hosts
+IP=$(ip a | grep 192 | awk '{print $2}' | awk -F/ '{print $1}')
 cat >> /etc/hosts << EOF
-$IP $HOSTNAME.internal.softwareheritage.org $HOSTNAME
+$IP $FQDN $HOSTNAME
 
 192.168.100.100 db
 192.168.100.101 uffizi
 192.168.100.31 moma
 EOF
 
-mkdir -p /etc/resolvconf/resolv.conf.d
-echo search internal.softwareheritage.org > /etc/resolvconf/resolv.conf.d/tail
-apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install resolvconf nfs-common
+# install puppet dependencies
+$APT install puppet
 
-apt-get update
+# do not need the service live as we need to install some more setup first
+service puppet stop
+systemctl disable puppet.service
 
-apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade
-apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install augeas-tools puppet
-
-
-# FIXME: Is this useful?
-augtool << "EOF"
-set /files/etc/puppet/puppet.conf/main/pluginsync true
-set /files/etc/puppet/puppet.conf/main/server pergamon.internal.softwareheritage.org
-save
-EOF
-
+# Install the location fact as that is needed by our puppet manifests
 mkdir -p /etc/facter/facts.d
 echo location=azure_euwest > /etc/facter/facts.d/location.txt
 
-service puppet stop
-systemctl disable puppet.service
-puppet agent --enable
+# first time around, this will:
+# - update the node's puppet agent configuration defining the puppet master
+# - generate the certificates with the appropriate fqdn
+# - unfortunately, for now, this fails though, when not being able to
+#   install the apt-transport-https package
+puppet agent --server $PUPPET_MASTER --waitforcert 60 --test
+# so we install it
+$APT install apt-transport-https
+# now everything should be fine
+puppet agent --server $PUPPET_MASTER --test || true
 
-augtool << "EOF"
-set /files/etc/puppet/puppet.conf/agent/server pergamon.internal.softwareheritage.org
-set /files/etc/puppet/puppet.conf/agent/report true
-set /files/etc/puppet/puppet.conf/agent/pluginsync true
-save
-EOF
-
-rm -rf /root/.ssh
-
-deluser testadmin
-rm -rf /home/testadmin
-
-puppet agent --test || true
-
-reboot
+#reboot
