09c81ee76e0b1b5453cb7afff9ffb42f169c6c31
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2018-08-03 15:56:47 +0200

    azure/add-disk.sh: Be consistent with the creation vm
    In regards to resource group, vm name computations.
    
    Ideally, this should be factorized.

--- azure/add-disk.sh
+++ azure/add-disk.sh
@@ -9,11 +9,25 @@ nodename=${1-"dbreplica1"}
 # type of node (worker, db, etc...)
 type=${2-"db"}
 
-# resource group
-resource_group="euwest-${type}"
-
 # not a choice
+resource_prefix="euwest"
 location=westeurope
+
+full_nodename="${nodename}-${resource_prefix}"
+
+# Depending on the types, we compute the resource group
+# worker, db, storage have dedicated shared resource group
+# other can be specifically tailored for them
+if [ $type = 'worker' ]; then   # for workers, it's a shared resource
+    resource_group="${resource_prefix}-${type}s"
+elif [ $type = 'db' ]; then     # for dbs as well
+    resource_group="${resource_prefix}-${type}"
+    full_nodename="${nodename}"
+elif [ $type = 'storage' ]; then
+    resource_group="${resource_prefix}-server"
+else # for other node types (webapp), that is specifically tailored for
+    resource_group="${resource_prefix}-${nodename}"
+fi
 
 # disk's size in gb
 disk_size=${3-1024}
@@ -25,7 +39,7 @@ disk_name="${nodename}_pgdata0"
 # this will create the disk if it does not exist
 cmd="az vm disk attach \
    --resource-group ${resource_group} \
-   --vm-name ${nodename} \
+   --vm-name ${full_nodename} \
    --disk ${disk_name} \
    --size-gb ${disk_size} \
    --new"
