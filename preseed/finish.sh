e12315ed11a4cd6d1320300cae3f7542b397b50c
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2018-08-29 12:02:56 +0200

    preseed/finish.sh: Update /etc/hosts to make the puppet run work

--- preseed/finish.sh
+++ preseed/finish.sh
@@ -2,7 +2,8 @@
 
 set -e
 
-PUPPET_MASTER=pergamon.internal.softwareheritage.org
+PUPPET_MASTER=pergamon
+PUPPET_MASTER_FQDN="$PUPPET_MASTER.internal.softwareheritage.org"
 LOCATION=sesi_rocquencourt
 
 apt-get update
@@ -14,9 +15,11 @@ systemctl disable puppet.service
 mkdir -p /etc/facter/facts.d
 echo "location=$LOCATION" > /etc/facter/facts.d/location.txt
 
+echo "192.168.100.29 $PUPPET_MASTER_FQDN $PUPPET_MASTER" >> /etc/hosts
+
 # first time around, this will:
 # - update the node's puppet agent configuration defining the puppet master
 # - generate the certificates with the appropriate fqdn
 # - unfortunately, for now, this fails though, when not being able to
 #   install the apt-transport-https package
-puppet agent --server $PUPPET_MASTER --waitforcert 60 --test
+puppet agent --server $PUPPET_MASTER_FQDN --waitforcert 60 --test
