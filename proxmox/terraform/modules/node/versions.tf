adbd7425f5098bb58ddf61440ded9c89f126f387
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2020-10-07 17:50:26 +0200

    Centralize versions into one versions.tf file
    This will avoid desynchronization.

--- proxmox/terraform/modules/node/versions.tf
+++ proxmox/terraform/modules/node/versions.tf
@@ -1,12 +1,1 @@
-
-terraform {
-  required_version = ">= 0.13"
-
-  required_providers {
-    proxmox = {
-      source = "local/telmate/proxmox"
-      version = "0.0.1"
-    }
-  }
-
-}
+../../versions.tf
