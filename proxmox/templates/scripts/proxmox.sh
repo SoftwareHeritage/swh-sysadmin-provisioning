f2fae17af485a01f6bfe1cef828b5ab01a93e273
Vincent SELLIER <vincent.sellier@softwareheritage.org>
2020-11-06 09:28:55 +0100

    adapt the debian buster template to be used by terraform
    - The current template is configured with a default lsi scsi adaptor
    and a scsi disk. Terraform is using a virtio disk and creates a new
    virtio disk one if not existing instead of extended the current
    scsi volume. Configuring the template wirh a virtio disk solves
    the problem.
    - a qemu-server upgrade has changed the boot order option behavior which
    led to an wrong definition of the boot devices by packer.
    It forces to perform a manual action during the template creation
    and to explicitly declare the cloud_init_storage_pool option
    because the default value is computed from the now not existing boot
    device. It will be fixed on a future packer release[1].
    
    [1]: https://github.com/hashicorp/packer/pull/10208
    
    Related to T2747

--- proxmox/templates/scripts/proxmox.sh
+++ proxmox/templates/scripts/proxmox.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -eu
 
-
-apt-get install -y cloud-init qemu qemu-guest-agent
+DEBIAN_FRONTEND=noninteractive apt-get install -y cloud-init qemu qemu-guest-agent
 
 # Update grub for a faster boot
 sed -i s'/GRUB_TIMEOUT = 5/GRUB_TIMEOUT = 0/' /etc/default/grub
