4c200c911615712e7ebe5ae030e63fab8aac6076
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2020-12-22 17:39:18 +0100

    Clean up production indirection
    This inlines the production/common.tf within the production/production.tf

--- proxmox/terraform/production/production.tf
+++ proxmox/terraform/production/production.tf
@@ -1,3 +1,16 @@
+locals {
+  config = {
+    dns                             = var.dns
+    domain                          = "internal.softwareheritage.org"
+    puppet_environment              = "production"
+    puppet_master                   = var.puppet_master
+    gateway_ip                      = "192.168.100.1"
+    user_admin                      = var.user_admin
+    user_admin_ssh_public_key       = var.user_admin_ssh_public_key
+    user_admin_ssh_private_key_path = var.user_admin_ssh_private_key_path
+  }
+}
+
 module "kelvingrove" {
   source = "../modules/node"
   config = local.config
