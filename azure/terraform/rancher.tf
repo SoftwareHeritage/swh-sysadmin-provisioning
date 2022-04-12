5292d8c407a5a1ccebf75571e91d4e1cf1c78948
Vincent SELLIER <vincent.sellier@softwareheritage.org>
2022-04-12 09:54:57 +0200

    rancher: Create an aks cluster dedicated to rancher
    It will allow to bootstrap the rancher manager without
    manually installing a kubernetes on premise.
    
    Related to T4046

---
+++ azure/terraform/rancher.tf
@@ -0,0 +1,28 @@
+resource "azurerm_resource_group" "rancher_rg" {
+  name     = "euwest-rancher"
+  location = "westeurope"
+
+  tags = {
+    environment = "rancher"
+  }
+}
+
+# kubernetes cluster for compute and storage
+module "rancher_aks_cluster" {
+  source         = "./modules/kubernetes"
+  cluster_name   = "euwest-rancher"
+  resource_group = azurerm_resource_group.rancher_rg.name
+
+  minimal_pool_count     = 1
+  maximal_pool_count     = 3
+  node_type              = "Standard_B2ms"
+  public_ip_provisioning = false
+
+  depends_on = [
+    azurerm_resource_group.rancher_rg
+  ]
+}
+
+output "rancher_aks_summary" {
+  value = module.rancher_aks_cluster.summary
+}
