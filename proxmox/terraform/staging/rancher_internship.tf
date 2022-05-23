6e7b13218cbcdbd7b8fd02f3e327f6924511ef67
Vincent SELLIER <vincent.sellier@softwareheritage.org>
2022-05-23 16:50:27 +0200

    rancher: downgrade deployment-internship cluster to 1.21
    Related to T4268

--- proxmox/terraform/staging/rancher_internship.tf
+++ proxmox/terraform/staging/rancher_internship.tf
@@ -1,27 +1,29 @@
 # - Create a rancher cluster
 # - Create 3 compute nodes
-resource "rancher2_cluster" "deployment_intership" {
-  name        = "deployment-intership"
+resource "rancher2_cluster" "deployment_internship" {
+  name        = "deployment-internship"
   description = "staging cluster for deployment test"
+
   rke_config {
+    kubernetes_version = "v1.21.12-rancher1-1"
     network {
       plugin = "canal"
     }
   }
 }
 
-output "deployment_intership_cluster_summary" {
+output "deployment_internship_cluster_summary" {
   sensitive = true
-  value     = rancher2_cluster.deployment_intership.kube_config
+  value     = rancher2_cluster.deployment_internship.kube_config
 }
 
-output "deployment_intership_cluster_command" {
+output "deployment_internship_cluster_command" {
   sensitive = true
-  value     = rancher2_cluster.deployment_intership.cluster_registration_token[0].node_command
+  value     = rancher2_cluster.deployment_internship.cluster_registration_token[0].node_command
 }
 
 module "rancher_node_internship0" {
-  hostname    = "rancher-node-intership0"
+  hostname = "rancher-node-intership0"
 
   source      = "../modules/node"
   template    = "debian-bullseye-11.3-zfs-2022-04-21"
@@ -53,7 +55,7 @@ module "rancher_node_internship0" {
 
   post_provision_steps = [
     "systemctl restart docker", # workaround
-    "${rancher2_cluster.deployment_intership.cluster_registration_token[0].node_command} --etcd --controlplane --worker"
+    "${rancher2_cluster.deployment_internship.cluster_registration_token[0].node_command} --etcd --controlplane --worker"
   ]
 }
 
@@ -62,7 +64,7 @@ output "rancher_node_internship0_summary" {
 }
 
 module "rancher_node_internship1" {
-  hostname    = "rancher-node-intership1"
+  hostname = "rancher-node-intership1"
 
   source      = "../modules/node"
   template    = "debian-bullseye-11.3-zfs-2022-04-21"
@@ -94,7 +96,7 @@ module "rancher_node_internship1" {
 
   post_provision_steps = [
     "systemctl restart docker", # workaround
-    "${rancher2_cluster.deployment_intership.cluster_registration_token[0].node_command} --etcd --controlplane --worker"
+    "${rancher2_cluster.deployment_internship.cluster_registration_token[0].node_command} --etcd --controlplane --worker"
   ]
 }
 
@@ -103,7 +105,7 @@ output "rancher_node_internship1_summary" {
 }
 
 module "rancher_node_internship2" {
-  hostname    = "rancher-node-intership2"
+  hostname = "rancher-node-intership2"
 
   source      = "../modules/node"
   template    = "debian-bullseye-11.3-zfs-2022-04-21"
@@ -135,7 +137,7 @@ module "rancher_node_internship2" {
 
   post_provision_steps = [
     "systemctl restart docker", # workaround
-    "${rancher2_cluster.deployment_intership.cluster_registration_token[0].node_command} --etcd --controlplane --worker"
+    "${rancher2_cluster.deployment_internship.cluster_registration_token[0].node_command} --etcd --controlplane --worker"
   ]
 }
 
