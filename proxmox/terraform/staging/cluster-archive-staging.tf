a905134d0393b95cf5b5395e3246540b909d06d2
Vincent SELLIER <vincent.sellier@softwareheritage.org>
2022-11-30 09:11:17 +0100

    k8s/archive-staging: Upgrade kubernetes cluster to 1.23
    Related to infra/sysadm-environment#4688

--- proxmox/terraform/staging/cluster-archive-staging.tf
+++ proxmox/terraform/staging/cluster-archive-staging.tf
@@ -7,7 +7,7 @@ resource "rancher2_cluster" "archive-staging" {
   name = "archive-staging"
   description = "Archive staging cluster"
   rke_config {
-    kubernetes_version = "v1.22.13-rancher1-1"
+    kubernetes_version = "v1.23.14-rancher1-1"
     network {
       plugin = "canal"
     }
