b07e37a20f338b1ecef6852d0f21595e1e5a046f
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2023-01-06 15:57:31 +0100

    Create archive-staging-rke2 cluster
    Related to swh/infra/sysadm-environment#4699

---
+++ proxmox/terraform/staging/cluster-archive-staging-rke2.tf
@@ -0,0 +1,143 @@
+resource "rancher2_cluster_v2" "archive-staging-rke2" {
+  name               = "archive-staging-rke2"
+  kubernetes_version = "v1.24.8+rke2r1"
+  rke_config {
+    upgrade_strategy {
+      worker_drain_options {
+        enabled               = true
+        delete_empty_dir_data = true
+        timeout               = 300
+      }
+    }
+  }
+}
+
+output "rancher2_cluster_archive_staging_rke2_summary" {
+  sensitive = true
+  value     = rancher2_cluster_v2.archive-staging-rke2.kube_config
+}
+
+output "rancher2_cluster_archive_staging_rke2_command" {
+  sensitive = true
+  value     = rancher2_cluster_v2.archive-staging-rke2.cluster_registration_token[0].node_command
+}
+
+module "rancher-node-staging-rke2-mgmt1" {
+  source      = "../modules/node"
+  template    = var.templates["stable-zfs"]
+  config      = local.config
+  hostname    = "rancher-node-staging-rke2-mgmt1"
+  description = "staging rke2 management node"
+  hypervisor  = "pompidou"
+  sockets     = "1"
+  cores       = "4"
+  onboot      = true
+  memory      = "8192"
+  balloon     = "8192"
+
+  networks = [{
+    id      = 0
+    ip      = "192.168.130.140"
+    gateway = local.config["gateway_ip"]
+    bridge  = local.config["bridge"]
+  }]
+
+  storages = [{
+    storage = "proxmox"
+    size    = "20G"
+    }, {
+    storage = "proxmox"
+    size    = "20G"
+    }
+  ]
+
+  post_provision_steps = [
+    "systemctl restart docker",  # workaround
+    "${rancher2_cluster_v2.archive-staging-rke2.cluster_registration_token[0].node_command} --etcd --controlplane"
+  ]
+}
+
+output "rancher-node-staging-rke2-mgmt1_summary" {
+  value = module.rancher-node-staging-rke2-mgmt1.summary
+}
+
+# Dedicated node for rpc services (e.g. graphql, ...)
+module "rancher-node-staging-rke2-worker1" {
+  source      = "../modules/node"
+  template    = var.templates["stable-zfs"]
+  config      = local.config
+  hostname    = "rancher-node-staging-rke2-worker1"
+  description = "elastic worker for rpc services (e.g. graphql, ...)"
+  hypervisor  = "pompidou"
+  sockets     = "1"
+  cores       = "4"
+  onboot      = true
+  memory      = "32768"
+  balloon     = "16384"
+
+  networks = [{
+    id      = 0
+    ip      = "192.168.130.141"
+    gateway = local.config["gateway_ip"]
+    bridge  = local.config["bridge"]
+  }]
+
+  storages = [{
+    storage = "proxmox"
+    size    = "20G"
+    }, {
+    storage = "scratch"
+    size    = "50G"
+    }
+  ]
+
+  post_provision_steps = [
+    "systemctl restart docker",  # workaround
+    "${rancher2_cluster.archive-staging.cluster_registration_token[0].node_command} --worker --label node_type=generic --label swh/rpc=true"
+  ]
+}
+
+output "rancher-node-staging-rke2-worker1_summary" {
+  value = module.rancher-node-staging-rke2-worker1.summary
+}
+
+# loader nodes must have a 2nd disk on hypervisor local storage to avoid
+# unnecessary ceph traffic on ceph
+module "rancher-node-staging-rke2-worker2" {
+  source      = "../modules/node"
+  template    = var.templates["stable-zfs"]
+  config      = local.config
+  hostname    = "rancher-node-staging-rke2-worker2"
+  description = "elastic worker for computations (e.g. loader, lister, ...)"
+  hypervisor  = "pompidou"
+  sockets     = "1"
+  cores       = "4"
+  onboot      = true
+  memory      = "32768"
+  balloon     = "16384"
+
+  networks = [{
+    id      = 0
+    ip      = "192.168.130.142"
+    gateway = local.config["gateway_ip"]
+    bridge  = local.config["bridge"]
+  }]
+
+  storages = [{
+    storage = "proxmox"
+    size    = "20G"
+    }, {
+    storage = "scratch"
+    size    = "50G"
+    }
+  ]
+
+  post_provision_steps = [
+    "systemctl restart docker",  # workaround
+    "${rancher2_cluster.archive-staging.cluster_registration_token[0].node_command} --worker --label node_type=worker --label swh/rpc=true --label swh/loader=true --label swh/lister=true"
+  ]
+}
+
+output "rancher-node-staging-rke2-worker2_summary" {
+  value = module.rancher-node-staging-rke2-worker2.summary
+}
