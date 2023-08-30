5f33bcf44bddeaa21d253754c9d101f64acb98b8
Guillaume Samson <guillaume.samson@softwareheritage.org>
2023-08-30 15:41:09 +0200

    proxmox/packer: Create Debian bookworm images
    Related to swh/infra/sysadm-environment#5015

--- proxmox/templates/scripts/post-install-extra.sh
+++ proxmox/templates/scripts/post-install-extra.sh
@@ -5,9 +5,14 @@
 ####
 source /etc/os-release
 
-cat <<EOF >/etc/apt/sources.list.d/backports.list
-deb http://deb.debian.org/debian/ bullseye-backports main contrib
+if [ ${VERSION_CODENAME} = "bookworm" ]; then
+    sed -i "s/${VERSION_CODENAME} main/${VERSION_CODENAME} main contrib/" /etc/apt/sources.list.d/debian.list
+    sed -i "s/${VERSION_CODENAME}-updates main/${VERSION_CODENAME}-updates main contrib/" /etc/apt/sources.list.d/debian.list
+else
+    cat <<EOF >/etc/apt/sources.list.d/backports.list
+deb http://deb.debian.org/debian/ ${VERSION_CODENAME}-backports main contrib
 EOF
+fi
 
 export DEBIAN_FRONTEND=noninteractive
 
--- proxmox/templates/scripts/proxmox.sh
+++ proxmox/templates/scripts/proxmox.sh
@@ -1,6 +1,14 @@
 #!/bin/bash -eu
 
-DEBIAN_FRONTEND=noninteractive apt-get install -y cloud-init qemu qemu-guest-agent
+source /etc/os-release
+
+export DEBIAN_FRONTEND=noninteractive
+
+if [ ${VERSION_CODENAME} = "bookworm" ]; then
+    apt-get install -y cloud-init qemu-guest-agent
+else
+    apt-get install -y cloud-init qemu qemu-guest-agent
+fi
 
 # Update grub for a faster boot
 sed -i s'/GRUB_TIMEOUT = 5/GRUB_TIMEOUT = 0/' /etc/default/grub
