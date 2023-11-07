d85b072e194440d00288ce528c93e84b11db0bab
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2023-11-07 10:29:53 +0100

    deposit: Add azure resources (resource group, storage account, ...)
    This adds the resource group, one staging storage account and an associated
    blob storage.
    
    This prepares the requirements for the deposit migration to the dynamic
    infra (in staging).  We are going to try and use an azure storage for the
    tarballs the deposit receives.
    
    Refs. swh/infra/sysadm-environment#5129

---
+++ azure/terraform/deposit.tf
@@ -0,0 +1,32 @@
+# Define a new resource for the deposit
+# matching what we name elsewhere "euwest-${resource}"
+
+resource "azurerm_resource_group" "euwest-deposit" {
+  name     = "euwest-deposit"
+  location = "westeurope"
+
+  tags = {
+    environment = "SWH Deposit"
+  }
+}
+
+resource "azurerm_storage_account" "deposit-storage-staging" {
+  name                      = "swhdepositstoragestaging"
+  resource_group_name       = azurerm_resource_group.euwest-deposit.name
+  location                  = "westeurope"
+  account_tier              = "Standard"
+  account_replication_type  = "LRS"
+  account_kind              = "BlobStorage"
+  access_tier               = "Cool"
+  enable_https_traffic_only = true
+  tags = {
+    environment = "SWH Deposit"
+  }
+}
+
+# A container for the blob storage named 'contents' (as other blob storages)
+resource "azurerm_storage_container" "deposit-contents" {
+  name                  = "deposit-contents"
+  storage_account_name  = azurerm_storage_account.deposit-storage-staging.name
+  container_access_type = "private"
+}
