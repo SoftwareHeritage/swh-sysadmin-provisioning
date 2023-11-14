83e8e081d6b11dbcfc1b38059a405e7dd81e7944
Vincent SELLIER <vincent.sellier@softwareheritage.org>
2023-11-14 09:30:16 +0100

    staging: decommission scrubber0
    ```
    terraform plan -out=/tmp/terraform.plan --destroy --target module.scrubber0.proxmox_vm_qemu.node
    terraform apply /tmp/terraform.plan
    ```
    
    Related to swh/infra/sysadm-environment#5145

--- proxmox/terraform/staging/staging.tf
+++ proxmox/terraform/staging/staging.tf
@@ -312,40 +312,6 @@ output "maven-exporter0_summary" {
   value = module.maven-exporter0.summary
 }
 
-module "scrubber0" {
-  source      = "../modules/node"
-  config      = local.config
-  hypervisor  = "pompidou"
-  onboot      = true
-
-  vmid        = 142
-  hostname    = "scrubber0"
-  description = "Scrubber checker services"
-  sockets     = "1"
-  cores       = "4"
-
-  memory  = "4096"
-  balloon = "1024"
-
-  networks = [{
-    id      = 0
-    ip      = "192.168.130.120"
-    gateway = local.config["gateway_ip"]
-    macaddr = "86:09:0A:61:AB:C1"
-    bridge  = local.config["bridge"]
-  }]
-
-  storages = [{
-    storage = "proxmox"
-    size    = "30G"
-    }
-  ]
-}
-
-output "scrubber0_summary" {
-  value = module.scrubber0.summary
-}
-
 module "runner0" {
   source      = "../modules/node"
   config      = local.config
