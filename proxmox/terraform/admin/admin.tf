6495efd7b2264acb2890ae1da2175637065abbaf
Nicolas Dandrimont <nicolas@dandrimont.eu>
2024-02-29 21:21:51 +0100

    admin: ensure only one locals definition is done

--- proxmox/terraform/admin/admin.tf
+++ proxmox/terraform/admin/admin.tf
@@ -1,19 +1,3 @@
-locals {
-  config = {
-    dns                             = var.dns
-    domain                          = "internal.admin.swh.network"
-    puppet_environment              = "production"
-    facter_deployment               = "admin"
-    facter_subnet                   = "sesi_rocquencourt_admin"
-    puppet_master                   = var.puppet_master
-    gateway_ip                      = "192.168.50.1"
-    user_admin                      = var.user_admin
-    user_admin_ssh_public_key       = var.user_admin_ssh_public_key
-    user_admin_ssh_private_key_path = var.user_admin_ssh_private_key_path
-    bridge                          = "vmbr442"
-  }
-}
-
 module "bardo" {
   source = "../modules/node"
   config = local.config
--- proxmox/terraform/admin/init.tf
+++ proxmox/terraform/admin/init.tf
@@ -9,7 +9,18 @@ terraform {
 
 locals {
   config = {
+    dns                             = var.dns
+    domain                          = "internal.admin.swh.network"
+    puppet_environment              = "production"
+    facter_deployment               = "admin"
+    facter_subnet                   = "sesi_rocquencourt_admin"
+    puppet_master                   = var.puppet_master
+    gateway_ip                      = "192.168.50.1"
+    user_admin                      = var.user_admin
+    user_admin_ssh_public_key       = var.user_admin_ssh_public_key
+    user_admin_ssh_private_key_path = var.user_admin_ssh_private_key_path
+    bridge                          = "vmbr442"
     # kubernetes cluster's max pods per node
-    max_pods_per_node               = 110
+    max_pods_per_node = 110
   }
 }
