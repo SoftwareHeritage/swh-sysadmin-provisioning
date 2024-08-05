7858e94656ed787544b2b162f1f0f213680a9f2b
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2024-08-05 16:18:38 +0200

    deposit: Create blobstorage for next-version deposit
    Refs. swh/infra/sysadm-environment#5344

--- azure/terraform/deposit.tf
+++ azure/terraform/deposit.tf
@@ -24,10 +24,33 @@ resource "azurerm_storage_account" "deposit-storage-staging" {
   }
 }
 
-# A container for the blob storage named 'deposit-contents' (as other blob storages)
 resource "azurerm_storage_container" "deposit-contents" {
+# A container for the blob storage named 'deposit-contents' (like other blob
+# storages)
   name                  = "deposit-contents"
   storage_account_name  = azurerm_storage_account.deposit-storage-staging.name
+  container_access_type = "blob"
+}
+
+resource "azurerm_storage_account" "deposit-storage-staging-next-version" {
+  name                      = "swhdepositstoragestgnxvs"
+  resource_group_name       = azurerm_resource_group.euwest-deposit.name
+  location                  = "westeurope"
+  account_tier              = "Standard"
+  account_replication_type  = "LRS"
+  account_kind              = "BlobStorage"
+  access_tier               = "Cool"
+  enable_https_traffic_only = true
+  tags = {
+    environment = "SWH Deposit"
+  }
+}
+
+# A container for the blob storage named 'deposit-contents' (like other blob
+# storages)
+resource "azurerm_storage_container" "deposit-contents-staging-next-version" {
+  name                  = "deposit-contents"
+  storage_account_name  = azurerm_storage_account.deposit-storage-staging-next-version.name
   container_access_type = "blob"
 }
 
@@ -45,7 +68,8 @@ resource "azurerm_storage_account" "deposit-storage" {
   }
 }
 
-# A container for the blob storage named 'deposit-contents' (as other blob storages)
+# A container for the blob storage named 'deposit-contents' (as other blob
+# storages)
 resource "azurerm_storage_container" "deposit-tarballs" {
   name                  = "deposit-tarballs"
   storage_account_name  = azurerm_storage_account.deposit-storage.name
