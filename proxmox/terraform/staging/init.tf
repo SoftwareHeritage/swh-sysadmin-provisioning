1bf01fcbba177c625ee0bb700ad707f04b030c64
Guillaume Samson <guillaume.samson@softwareheritage.org>
2025-02-20 15:33:06 +0100

    k8s/staging: Set max-pod to 200

--- proxmox/terraform/staging/init.tf
+++ proxmox/terraform/staging/init.tf
@@ -23,6 +23,6 @@ locals {
     user_admin_ssh_private_key_path = var.user_admin_ssh_private_key_path
     bridge                          = "vmbr443"
     # kubernetes cluster's max pods per node
-    max_pods_per_node               = 120
+    max_pods_per_node               = 200
   }
 }
