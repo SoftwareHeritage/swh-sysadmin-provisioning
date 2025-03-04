6d6caae41c4b8fc6431768b8d9ab69f6a8400e3d
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2025-03-04 16:09:52 +0100

    production/jenkins-docker01: Migrate configuration to node_bpg
    Refs. swh/infra/sysadm-environment#5426

--- proxmox/terraform/production/production.tf
+++ proxmox/terraform/production/production.tf
@@ -121,33 +121,39 @@ output "maven-exporter_summary" {
 }
 
 module "jenkins-docker01" {
-  source      = "../modules/node"
-  template    = var.templates["bullseye-zfs"]
-  vmid        = 151
+  source      = "../modules/node_bpg"
   config      = local.config
+  hypervisor  = "mucem"
+  onboot      = true
+  vmid        = 151
   hostname    = "jenkins-docker01"
   description = "Docker-based jenkins agent"
-  hypervisor  = "mucem"
-  cpu         = "host"
-  sockets     = "2"
-  cores       = "4"
-  onboot      = true
-  memory      = "32768"
-  balloon     = "2048"
 
-  networks = [{
-    id      = 0
-    ip      = "192.168.100.151"
-    gateway = local.config["gateway_ip"]
-    bridge  = local.config["bridge"]
-  }]
+  cpu = {
+    type    = "host"
+    sockets = 2
+    cores   = 4
+  }
 
-  storages = [{
-    storage = "proxmox"
-    size    = "20G"
-    }, {
-    storage = "scratch"
-    size    = "200G"
+  ram = {
+    dedicated = 32768
+    floating  = 2048
+  }
+
+  network = {
+    ip          = "192.168.100.151"
+    mac_address = "DA:1C:7D:C6:31:0E"
+  }
+
+  disks = [
+    {
+      interface = "virtio0"
+      size    = 20
+    },
+    {
+      datastore_id = "scratch"
+      interface    = "virtio1"
+      size         = 200
     }
   ]
 }
