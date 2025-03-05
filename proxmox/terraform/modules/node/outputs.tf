7a21413255494fcdde4f6bca94915cb3a24c7f0e
Guillaume Samson <guillaume.samson@softwareheritage.org>
2025-03-05 17:53:37 +0100

    terraform/proxmox: Clean up telmate provider
    Related to swh/infra/sysadm-environment#5426

--- proxmox/terraform/modules/node/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "summary" {
-  value = <<EOF
-
-hostname: ${proxmox_vm_qemu.node.name}
-fqdn: ${proxmox_vm_qemu.node.name}.${var.config["domain"]}
-network: ${proxmox_vm_qemu.node.ipconfig0}
-EOF
-
-}
