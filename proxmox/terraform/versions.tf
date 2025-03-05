7d55339f22f8a6cb035c9abd9fa1254bdc79d185
Guillaume Samson <guillaume.samson@softwareheritage.org>
2025-03-05 17:53:37 +0100

    terraform/proxmox: Clean up the former telmate provider
    Related to swh/infra/sysadm-environment#5426

--- proxmox/terraform/versions.tf
+++ proxmox/terraform/versions.tf
@@ -1,10 +1,6 @@
 terraform {
   required_version = ">= 0.13"
   required_providers {
-    proxmox = {
-      source = "telmate/proxmox"
-      version = "2.9.10"
-    }
     bpg-proxmox = {
       source = "bpg/proxmox"
       version = "0.73.0"
@@ -16,20 +12,6 @@ terraform {
   }
 }
 
-provider "proxmox" {
-  pm_tls_insecure = true
-  pm_api_url      = "https://mucem.internal.softwareheritage.org:8006/api2/json"
-  # in a shell (see README): source ../setup.sh
-
-  # Uncomment this section to activate the proxmox execution logs
-  # pm_log_enable = true
-  # pm_log_file = "terraform-plugin-proxmox.log"
-  # pm_debug = true
-  # pm_log_levels = {
-  #   _default = "debug"
-  #   _capturelog = ""
-  # }
-}
 provider "bpg-proxmox" {
   endpoint = "https://chaillot.internal.softwareheritage.org:8006/"
 }
